54a5c6c1d028851626ae69ee61fc3722630584d0
vishu118 <114770536+vishu118@users.noreply.github.com>
2023-04-06 19:16:28 +0530

    main page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import hotBg from "./Assets/hot.jpg"
+import coldBg from "./Assets/cold.jpg";
+
+
 
 function App() {
+  // const [city, setCity] = useState("Paris");
+  // const [weather, setWeather] = useState(null);
+  // const [units, setUnits] = useState("metric");
+  // const [bg, setBg] = useState(hotBg);
+
+  // useEffect(() => {
+  //   const fetchWeatherData = async () => {
+  //     const data = await getFormattedWeatherData(city, units);
+  //     setWeather(data);
+
+  //     // dynamic bg
+  //     const threshold = units === "metric" ? 20 : 60;
+  //     if (data.temp <= threshold) setBg(coldBg);
+  //     else setBg(hotBg);
+  //   };
+
+  //   fetchWeatherData();
+  // }, [units, city]);
+
+  // const handleUnitsClick = (e) => {
+  //   const button = e.currentTarget;
+  //   const currentUnit = button.innerText.slice(1);
+
+  //   const isCelsius = currentUnit === "C";
+  //   button.innerText = isCelsius ? "°F" : "°C";
+  //   setUnits(isCelsius ? "metric" : "imperial");
+  // };
+
+  // const enterKeyPressed = (e) => {
+  //   if (e.keyCode === 13) {
+  //     setCity(e.currentTarget.value);
+  //     e.currentTarget.blur();
+  //   }
+  // };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app" style={{ backgroundImage: `url(${coldBg})` }}>
+      <div className="overlay">
+          <div className="container">
+            <div className="section section__inputs">
+              <input
+                type="text"
+                name="city"
+                placeholder="Enter City..."
+              />
+              <button>°F</button>
+            </div>
+
+            <div className="section section__temperature">
+              <div className="icon">
+                <h3>City</h3>
+                <img src="" alt="weatherIcon" />
+                <h3>cloudy</h3>
+              </div>
+              <div className="temperature">
+                <h1>34°C</h1>
+              </div>
+            </div>
+
+            {/* bottom description */}
+           
+          </div>
+      </div>
     </div>
   );
 }
